f42811e45c2ac8d3f46daa0a741bd99836174ff3
Leonardo Worm Vieira <100496238+Manioco@users.noreply.github.com>
2024-04-05 17:59:29 -0300

    test

---
+++ module/area.js
@@ -0,0 +1,9 @@
+// Exporting function as separated things
+
+exports.area = function(width) {
+    return width * width
+}
+
+exports.perimeter = function(width) {
+    return 4 * width
+}
---
+++ module/twoValues.js
@@ -0,0 +1,11 @@
+// Export module as a complete object
+
+module.export = {
+    sumTwo(a, b){
+        return a + b
+    },
+
+    subTwo(a , b) {
+        return a - b
+    }
+}
